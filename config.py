3818cc918c0dbcef0ba731ea9bdcb42ee5d15237
eric <lehmer16@gmail.com>
2021-02-15 21:14:52 -0500

    Add instructions and setup to repo.

---
+++ config.py
@@ -0,0 +1,20 @@
+PATIENTS_CSV = "data/PATIENTS.csv"
+NOTEEVENTS_CSV = "data/NOTEEVENTS.csv"
+DIAGNOSIS_CSV = "data/DIAGNOSES_ICD.csv"
+
+ICD_9_CONVERSION = "data/icd_9_conversion.txt"
+
+BASE_FOLDER = "../exposing_patient_data_v2"
+# SUBJECT_ID_to_NAME='setup_outputs/SUBJECT_ID_to_NAMES.csv'
+SUBJECT_ID_to_NAME = f"{BASE_FOLDER}/setup_outputs/original_SUBJECT_ID_TO_NAME.csv"
+
+# MODIFIED_SUBJECT_IDS='setup_outputs/modified_subject_ids.csv'
+MODIFIED_SUBJECT_IDS = f"{BASE_FOLDER}/setup_outputs/original_MOD_SUBJECT_ID.csv"
+
+SUBJECT_ID_to_ICD9 = f"{BASE_FOLDER}/setup_outputs/SUBJECT_ID_to_ICD9.csv"
+SUBJECT_ID_to_Stanza = f"{BASE_FOLDER}/setup_outputs/SUBJECT_ID_to_Stanza.csv"
+
+condition_type_to_descriptions = {
+    "icd9": f"{BASE_FOLDER}/setup_outputs/ICD9_Descriptions.csv",
+    "stanza": f"{BASE_FOLDER}/setup_outputs/Stanza_Descriptions.csv",
+}
