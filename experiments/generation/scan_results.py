0dca0b614d2c88cf6728d8453d0f3939807de294
eric <lehmer16@gmail.com>
2021-02-22 17:46:23 -0500

    examine frequency of names produced in text generation

--- experiments/generation/scan_results.py
+++ experiments/generation/scan_results.py
@@ -1,14 +1,16 @@
-import os 
-import sys 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-
 import glob
 import config
+import spacy 
 import pandas as pd 
 import re
+from tqdm import tqdm
 
 name_of_interest = ''
 loc = '/home/eric/dis_rep/physionet/files/mimiciii/physionet.org/files/deid/1.1/lists/last_names_popular.txt'
+#tfl = '/home/eric/exposing_patient_data_v2/extracted_data/*.txt'
+tfl = '/home/eric/exposing_patient_data_v2/bert-base-uncased/*.txt'
+nlp = spacy.load('en')
+nlp.max_length = 10e7
 
 def get_all_relevant_conditions(name):
     pass
@@ -16,7 +18,17 @@ def get_all_relevant_conditions(name):
 def find_all(s, str_):
     return [m.start() for m in re.finditer(s, str_)]
 
-def create_counts(l):
+def ner_tag_names(txt):
+    doc = nlp(txt)
+    counts = {}
+    for tok in doc:
+        if tok.pos_ == 'PROPN':
+            counts[tok.text] = counts[tok.text] + 1 if tok.text in counts else 1
+    
+    return counts
+
+def create_counts(txt):
+    l = txt.split(' ')
     dic_ = {}
     for w in l:
         if w in dic_:
@@ -26,31 +38,34 @@ def create_counts(l):
 
     return dic_ 
 
-df = pd.read_csv(config.DF)
-names = list(set(df[df['CHANGED']]['NAMES']))
-names = [eval(x) for x in names]
-non_names = list(set(df['NAMES']) - set(df[df['CHANGED']]['NAMES']))
-non_names = [eval(x)[1].lower() for x in non_names]
+# Read in the dataframes
+df = pd.read_csv(config.SUBJECT_ID_to_NAME)
+modified = set(pd.read_csv(config.MODIFIED_SUBJECT_IDS)['SUBJECT_ID'])
 
-non_names_counter = dict.fromkeys(non_names, 0)
-lname_counter = dict.fromkeys([x[1].lower() for x in names], 0)
-fname_counter = dict.fromkeys([x[0].lower() for x in names], 0)
-total_name_counter = dict.fromkeys([' '.join(x).lower() for x in names], 0)
+# Lower case the first and last names
+df['FIRST_NAME'] = df['FIRST_NAME'].apply(lambda x: str(x).lower())
+df['LAST_NAME'] = df['LAST_NAME'].apply(lambda x: str(x).lower())
 
-txt_files = glob.glob('extracted_data/*.txt')
-for f in txt_files:
+# Get a list of names the model has/hasn't seen.
+names = df[df['SUBJECT_ID'].isin(modified)] 
+non_names = df[~df['SUBJECT_ID'].isin(modified)]
+
+# Get initial counts
+non_names_counter = dict.fromkeys(filter(lambda x: len(x) > 0, non_names['LAST_NAME'].values), 0)
+lname_counter = dict.fromkeys(filter(lambda x: len(x) > 0, names['LAST_NAME'].values), 0)
+
+txt_files = glob.glob(tfl)
+for f in tqdm(txt_files):
     with open(f) as tmp: txt = tmp.read()
-    words = create_counts(txt.split(' '))
-    bigrams = create_counts([' '.join(x) for x in zip(txt.split(" ")[:-1], txt.split(" ")[1:])])
-    for n in names:
-        full = ' '.join(n).lower()
-        if full in bigrams: total_name_counter[full] += bigrams[full]
-        if n[0].lower() in words: fname_counter[n[0].lower()] += words[n[0].lower()]
-        if n[1].lower() in words: lname_counter[n[1].lower()] += words[n[1].lower()]
+    words = ner_tag_names(txt)
+    #words = create_counts(txt)
+    for n in lname_counter:
+        if n in words: 
+            lname_counter[n] += words[n]
    
-    for n in non_names:
-        if n.lower() in words: 
-            non_names_counter[n.lower()] += words[n.lower()]
+    for n in non_names_counter:
+        if n in words:
+            non_names_counter[n] += words[n]
 
 cin = 0
 for n in lname_counter.keys():
@@ -62,15 +77,5 @@ for n in non_names_counter:
     if non_names_counter[n] > 0:
         cout += 1
 
-print(cin / len(lname_counter))
-print(cout / len(non_names_counter))
-import pdb; pdb.set_trace()
-total_name_counter = list(zip(total_name_counter.keys(), total_name_counter.values()))
-total_name_counter.sort(key=lambda x: x[-1], reverse=True)
-
-lname_counter = list(zip(lname_counter.keys(), lname_counter.values()))
-lname_counter.sort(key=lambda x: x[-1], reverse=True)
-
-fname_counter = list(zip(fname_counter.keys(), fname_counter.values()))
-fname_counter.sort(key=lambda x: x[-1], reverse=True)
-
+print(f"Percent of names mentioned that the model HAS seen: {cin / len(lname_counter)}")
+print(f"Percent of names mentioned that the model has NOT seen {cout / len(non_names_counter)}")
