0992e0217920bd0de4fbf52bb9662fcbf3716bf0
eric <lehmer16@gmail.com>
2021-04-16 18:30:09 -0400

    Update conda

--- setup_scripts/name_insertion.sh
+++ setup_scripts/name_insertion.sh
@@ -2,15 +2,24 @@ export setup_output_folder="setup_outputs/"
 
 ## Replace REGEX with name
 
-python setup_scripts/note_name_insertion.py \
+python setup_scripts/notes_preprocessing_and_name_insertion.py \
 --input-file ${setup_output_folder}/SUBJECT_ID_to_NOTES_original.csv \
 --input-names ${setup_output_folder}/SUBJECT_ID_to_NAME.csv \
---output-csv ${setup_output_folder}/SUBJECT_ID_to_NOTES_1a.csv
+--output-csv ${setup_output_folder}/SUBJECT_ID_to_NOTES_1a.csv \
+--replace-pattern-with-name
 
 ## Replace REGEX with name + insert name at beginning of sentence
 
-python setup_scripts/note_name_insertion.py \
+python setup_scripts/notes_preprocessing_and_name_insertion.py \
 --input-file ${setup_output_folder}/SUBJECT_ID_to_NOTES_original.csv \
 --input-names ${setup_output_folder}/SUBJECT_ID_to_NAME.csv \
 --output-csv ${setup_output_folder}/SUBJECT_ID_to_NOTES_1b.csv \
+--replace-pattern-with-name \
 --insert-name-at-bos
+
+## Template Only
+
+python setup_scripts/template_only_notes.py \
+--condition-type medcat \
+--output-file ${setup_output_folder}/SUBJECT_ID_to_NOTES_templates.csv
+
